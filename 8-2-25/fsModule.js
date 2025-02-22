a2301dd36a23cb6cec55d04e31bced41d2b508c9
dumpalavaishnavi <dumpalavaishnavi@gmail.com>
2025-02-22 11:38:07 +0530

    Delete 8-2-25 directory

--- 8-2-25/fsModule.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-
-fs.writeFile("demo.txt","Good Bye",(err,data)=>{
-    console.log("Data written succesfully");
-});
-
-fs.readFile("demo.txt","UTF-8",(err,data)=>{
-    console.log(data);
-})
